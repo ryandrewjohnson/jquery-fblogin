6aab11f9d469a59bd75493932fef827d7a941a9f
ryanjohnson <ryan@shakainteractive.com>
2014-07-28 15:54:34 -0400

    add bower.json file

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,11 @@
 (function($) {
+    
+    module('plugin options');
+    test('required FB id option not included', function () {
+        throws(function () {
+            $.fblogin();
+        },
+        'Should throw an error');
+    });
 
 }(jQuery));
